7c1c94c39672e1fe84ca48bc4693418532264542
gs.lee <gs.lee@cdnetworks.co.kr>
2021-02-02 19:17:30 +0900

    align, headerAlign 추가하여 각 properties마다 align 지정 jsonToHtml function 외부사용 함수로 변경

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './sass/index.scss'
 import print from './js/init'
+import Json from './js/json'
 
 const printJS = print.init
+printJS.jsonToHTML = Json.jsonToHTML
 
 if (typeof window !== 'undefined') {
   window.printJS = printJS
--- src/js/json.js
+++ src/js/json.js
@@ -1,7 +1,7 @@
 import { capitalizePrint, addHeader } from './functions'
 import Print from './print'
 
-export default {
+const Json = {
   print: (params, printFrame) => {
     // Check if we received proper data
     if (typeof params.printable !== 'object') {
@@ -23,7 +23,9 @@ export default {
       return {
         field: typeof property === 'object' ? property.field : property,
         displayName: typeof property === 'object' ? property.displayName : property,
-        columnSize: typeof property === 'object' && property.columnSize ? property.columnSize + ';' : 100 / params.properties.length + '%;'
+        columnSize: typeof property === 'object' && property.columnSize ? 'width:' + property.columnSize + ';' : 'width:' + (100 / params.properties.length) + '%;',
+        align: typeof property === 'object' && property.align ? 'text-align:' + property.align + ';' : '',
+        headerAlign: typeof property === 'object' && property.headerAlign ? 'text-align:' + property.headerAlign + ';' : '',
       }
     })
 
@@ -36,74 +38,76 @@ export default {
     }
 
     // Build the printable html data
-    params.printableElement.innerHTML += jsonToHTML(params)
+    params.printableElement.innerHTML += Json.jsonToHTML(params)
 
     // Print the json data
     Print.send(params, printFrame)
-  }
-}
+  },
 
-function jsonToHTML (params) {
-  // Get the row and column data
-  const data = params.printable
-  const properties = params.properties
-
-  // Create a html table
-  let htmlData = '<table style="border-collapse: collapse; width: 100%;">'
-
-  // Check if the header should be repeated
-  if (params.repeatTableHeader) {
-    htmlData += '<thead>'
-  }
-
-  // Add the table header row
-  htmlData += '<tr>'
-
-  // Add the table header columns
-  for (let a = 0; a < properties.length; a++) {
-    htmlData += '<th style="width:' + properties[a].columnSize + ';' + params.gridHeaderStyle + '">' + capitalizePrint(properties[a].displayName) + '</th>'
-  }
-
-  // Add the closing tag for the table header row
-  htmlData += '</tr>'
-
-  // If the table header is marked as repeated, add the closing tag
-  if (params.repeatTableHeader) {
-    htmlData += '</thead>'
-  }
-
-  // Create the table body
-  htmlData += '<tbody>'
-
-  // Add the table data rows
-  for (let i = 0; i < data.length; i++) {
-    // Add the row starting tag
-    htmlData += '<tr>'
-
-    // Print selected properties only
-    for (let n = 0; n < properties.length; n++) {
-      let stringData = data[i]
-
-      // Support nested objects
-      const property = properties[n].field.split('.')
-      if (property.length > 1) {
-        for (let p = 0; p < property.length; p++) {
-          stringData = stringData[property[p]]
-        }
-      } else {
-        stringData = stringData[properties[n].field]
-      }
-
-      // Add the row contents and styles
-      htmlData += '<td style="width:' + properties[n].columnSize + params.gridStyle + '">' + stringData + '</td>'
+  jsonToHTML: function(params) {
+    // Get the row and column data
+    const data = params.printable
+    const properties = params.properties
+  
+    // Create a html table
+    let htmlData = '<table style="border-collapse: collapse; width: 100%;">'
+  
+    // Check if the header should be repeated
+    if (params.repeatTableHeader) {
+      htmlData += '<thead>'
     }
-
-    // Add the row closing tag
+  
+    // Add the table header row
+    htmlData += '<tr>'
+  
+    // Add the table header columns
+    for (let n = 0; n < properties.length; n++) {
+      htmlData += '<th style="' + properties[n].columnSize + params.gridHeaderStyle + ';' + properties[n].headerAlign + '">' + capitalizePrint(properties[n].displayName) + '</th>'
+    }
+  
+    // Add the closing tag for the table header row
     htmlData += '</tr>'
-  }
-
-  // Add the table and body closing tags
-  htmlData += '</tbody></table>'
-
-  return htmlData
+  
+    // If the table header is marked as repeated, add the closing tag
+    if (params.repeatTableHeader) {
+      htmlData += '</thead>'
+    }
+  
+    // Create the table body
+    htmlData += '<tbody>'
+  
+    // Add the table data rows
+    for (let i = 0; i < data.length; i++) {
+      // Add the row starting tag
+      htmlData += '<tr>'
+  
+      // Print selected properties only
+      for (let n = 0; n < properties.length; n++) {
+        let stringData = data[i]
+  
+        // Support nested objects
+        const property = properties[n].field.split('.')
+        if (property.length > 1) {
+          for (let p = 0; p < property.length; p++) {
+            stringData = stringData[property[p]]
+          }
+        } else {
+          stringData = stringData[properties[n].field]
+        }
+  
+        // Add the row contents and styles
+        htmlData += '<td style="' + properties[n].columnSize + params.gridStyle + ';' + properties[n].align + '">' + stringData + '</td>'
+      }
+  
+      // Add the row closing tag
+      htmlData += '</tr>'
+    }
+  
+    // Add the table and body closing tags
+    htmlData += '</tbody></table>'
+  
+    return htmlData
+  } 
 }
+
+export default Json
